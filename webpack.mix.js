2bb57d2bf898f0a06f6e591590b100686ef107c8
svendehens <dehens.sven@gmail.com>
2021-10-29 17:38:22 +0200

    Moved old code to seperate folder.

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,20 +18,21 @@ mix.js('resources/js/app.js', 'public/js').vue()
         require('postcss-import'),
         require('tailwindcss'),
     ])
-    .js(['resources/js/Machine/vendor.js', 'resources/js/Machine/jquery.jsPlumb-1.3.16-all.js'], 'public/js/machine/vendor.js')
+    .js(['resources/js/Machine_original/vendor.js', 'resources/js/Machine_original/jquery.jsPlumb-1.3.16-all.js'], 'public/js/machine_original/vendor.js')
+    .js('resources/js/Machine/app.js', 'public/js')
     .combine([
-        'resources/js/Machine/utils.js',
-        'resources/js/Machine/app.js',
-        'resources/js/Machine/functions.js',
-        'resources/js/Machine/grid.js',
-        'resources/js/Machine/txtwiki.js',
-        'resources/js/Machine/dragscrollable.js',
-        'resources/js/Machine/wikify.js',
-        'resources/js/Machine/jquery.highlight-3.js',
-        'resources/js/Machine/jquery.ba-dotimeout.min.js',
-        'resources/js/Machine/jquery.jscrollpane.min.js',
+        'resources/js/Machine_original/utils.js',
+        'resources/js/Machine_original/app.js',
+        'resources/js/Machine_original/functions.js',
+        'resources/js/Machine_original/grid.js',
+        'resources/js/Machine_original/txtwiki.js',
+        'resources/js/Machine_original/dragscrollable.js',
+        'resources/js/Machine_original/wikify.js',
+        'resources/js/Machine_original/jquery.highlight-3.js',
+        'resources/js/Machine_original/jquery.ba-dotimeout.min.js',
+        'resources/js/Machine_original/jquery.jscrollpane.min.js',
     ], 'public/js/machine/app.js')
-    .copy('resources/js/Machine/jquery.window.min.js', 'public/js/machine')
+    .copy('resources/js/Machine_original/jquery.window.min.js', 'public/js/machine')
     .postCss('resources/css/machine.css', 'public/css', [
         require('postcss-import'),
     ])
